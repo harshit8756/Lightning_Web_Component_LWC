db702fc465e39c7c33a151f1e9572dc81de30905
hayadav <harshit.yadav@soprasteria.com>
2026-08-05 12:23:21 +0530

    Updpating the files

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -1,3 +1,8 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
 
-export default class ChildComponent extends LightningElement {}
+export default class ChildComponent extends LightningElement {
+
+    @api productId;
+    @api productName;
+    @api productRating;
+}
