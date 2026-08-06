8d664382e4ffc166a665ce79ce3d874738783b4b
HARSHIT YADAV <govindchandrayadav7379@gmail.com>
2026-08-06 19:55:50 +0530

    connected to the laptop

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -2,6 +2,7 @@ import { api, LightningElement } from 'lwc';
 
 export default class ChildComponent extends LightningElement {
 
+    //connection to the Personal Laptop
     @api productId;
     @api productName;
     @api productRating;
