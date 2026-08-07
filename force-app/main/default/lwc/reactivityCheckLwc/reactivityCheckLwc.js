88949b631e0ca17e40bf149e9b94ff2549ff10c0
hayadav <harshit.yadav@soprasteria.com>
2026-08-07 18:24:40 +0530

    child component is created and show the reactivity around the child level from parent level

--- force-app/main/default/lwc/reactivityCheckLwc/reactivityCheckLwc.js
+++ force-app/main/default/lwc/reactivityCheckLwc/reactivityCheckLwc.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class ReactivityCheckLwc extends LightningElement {}
+export default class ReactivityCheckLwc extends LightningElement {
+    userAddress = '123 Main St, Springfield, USA';
+
+    // create method for the update address value and check reacactivity of the component
+    handleAddressUpdate(){
+        this.userAddress = '456 Elm St, Springfield, USA';
+    }
+}
