f05cb2a76d7eb3f24d44b617d135bfcb7a2f77c0
hayadav <harshit.yadav@soprasteria.com>
2026-08-11 16:13:15 +0530

    initial commut

--- force-app/main/default/lwc/importUtilityLWC/importUtilityLWC.js
+++ force-app/main/default/lwc/importUtilityLWC/importUtilityLWC.js
@@ -2,14 +2,23 @@ import { LightningElement } from 'lwc';
 import mylogo from "@salesforce/resourceUrl/AgentforceLogo";
 import customLabels from "@salesforce/label/c.Show_in_Production";
 import svgVariable from "@salesforce/resourceUrl/SvgResources";
+import USERWITHID from '@salesforce/user/Id';
+import HASPERMISSION from "@salesforce/userPermission/ViewSetup";
 
 export default class ImportUtilityLWC extends LightningElement {
     src = mylogo;
     //showInProduction = false;
     svrExample = svgVariable;
+    user = USERWITHID;
+    //permission = HASPERMISSION;
 
     get showInProd() {
         return customLabels == "true" ? true : false;
+    }
+
+    get hasPermission(){
+        console.log('Imported from permission set : ' + HASPERMISSION);
+        return HASPERMISSION;
     }
 
 }
