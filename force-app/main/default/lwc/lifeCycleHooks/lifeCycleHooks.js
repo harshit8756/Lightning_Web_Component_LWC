fe2c529a3fe485bbc21d66df67410bdc6acf8658
hayadav <harshit.yadav@soprasteria.com>
2026-08-12 21:31:43 +0530

    lifecyclehooks

--- force-app/main/default/lwc/lifeCycleHooks/lifeCycleHooks.js
+++ force-app/main/default/lwc/lifeCycleHooks/lifeCycleHooks.js
@@ -1,3 +1,28 @@
 import { LightningElement } from 'lwc';
+export default class LifeCycleHooks extends LightningElement {
+    userName = 'Life Cycle Hooks';
 
-export default class LifeCycleHooks extends LightningElement {}
+    handleNameChange(){
+        this.userName = 'Changes has been Happened';
+    }
+
+    constructor(){
+        super();
+        console.log('Constructor is called');
+    }
+
+    connectedCallback(){
+        console.log('Connected Callback is called');
+    }
+
+    renderedCallback(){
+        console.log('Rendered Callback is called');
+    }
+
+    disconnectedCallback(){
+        console.log('Disconnected Callback is called when is done with it was supposed to do');
+    }
+    errorCallback(stack, trace){
+        console.log(stack + '----' + trace);
+    }
+}
