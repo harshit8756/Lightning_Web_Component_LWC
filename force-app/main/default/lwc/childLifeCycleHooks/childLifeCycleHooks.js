2ffff05dc765db8fd1f675ca5cc661a38ebb6443
hayadav <harshit.yadav@soprasteria.com>
2026-08-13 19:41:52 +0530

    parent child lifecycle hooks communication

--- force-app/main/default/lwc/childLifeCycleHooks/childLifeCycleHooks.js
+++ force-app/main/default/lwc/childLifeCycleHooks/childLifeCycleHooks.js
@@ -1,3 +1,23 @@
 import { LightningElement } from 'lwc';
 
-export default class ChildLifeCycleHooks extends LightningElement {}
+export default class ChildLifeCycleHooks extends LightningElement {
+    constructor(){
+        super();
+        console.log('Child - Constructor is called');
+    }
+
+    connectedCallback(){
+        console.log('Child - Connected Callback is called');
+    }
+
+    renderedCallback(){
+        console.log('Child - Rendered Callback is called');
+    }
+
+    disconnectedCallback(){
+        console.log('Child - Disconnected Callback is called when is done with it was supposed to do');
+    }
+    errorCallback(stack, trace){
+        console.log('Child -' +  stack + '----' + trace);
+    }
+}
--- force-app/main/default/lwc/parentLifeCycleHooks/parentLifeCycleHooks.js
+++ force-app/main/default/lwc/parentLifeCycleHooks/parentLifeCycleHooks.js
@@ -1,3 +1,23 @@
 import { LightningElement } from 'lwc';
 
-export default class ParentLifeCycleHooks extends LightningElement {}
+export default class ParentLifeCycleHooks extends LightningElement {
+    constructor(){
+        super();
+        console.log('Parent - Constructor is called');
+    }
+
+    connectedCallback(){
+        console.log('Parent - Connected Callback is called');
+    }
+
+    renderedCallback(){
+        console.log('Parent - Rendered Callback is called');
+    }
+
+    disconnectedCallback(){
+        console.log('Parent - Disconnected Callback is called when is done with it was supposed to do');
+    }
+    errorCallback(stack, trace){
+        console.log('Parent -' + stack + '----' + trace);
+    }
+}
