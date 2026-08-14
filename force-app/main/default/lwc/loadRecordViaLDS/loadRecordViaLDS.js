aa408c0dddf88533d7aca78eb26e192c00122270
hayadav <harshit.yadav@soprasteria.com>
2026-08-14 19:57:42 +0530

    lds

--- force-app/main/default/lwc/loadRecordViaLDS/loadRecordViaLDS.js
+++ force-app/main/default/lwc/loadRecordViaLDS/loadRecordViaLDS.js
@@ -1,3 +1,11 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
 
-export default class LoadRecordViaLDS extends LightningElement {}
+export default class LoadRecordViaLDS extends LightningElement {
+
+    //creating variable in the js
+    //recordId = '500gK00000AIKIfQAP'; // this is the hard coded record id of the Case object , working as static
+    //for Dynamic we expose this variable at public level so we use the @api decorator
+    @api recordId;
+    objectApiName = 'Case';
+    fields = ['CaseNumber','Priority','Status']
+}
