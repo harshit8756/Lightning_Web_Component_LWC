d56294952992fceed0bd6717e9822c445dbcac39
hayadav <113333121+harshit8756@users.noreply.github.com>
2026-08-18 14:54:11 +0530

    rest record via LDS

--- force-app/main/default/lwc/editRecordViaLightningRecordEditFormLDS/editRecordViaLightningRecordEditFormLDS.js
+++ force-app/main/default/lwc/editRecordViaLightningRecordEditFormLDS/editRecordViaLightningRecordEditFormLDS.js
@@ -3,5 +3,13 @@ import { api, LightningElement } from 'lwc';
 export default class EditRecordViaLightningRecordViewFormLDS extends LightningElement {
     @api recordId;
     objectApiName = 'Case';
+
+    handleFormSuccess(event){
+        // as soon as success happen resest the form , we have form + reset
+        console.log('Success Called and invoked');
+        const formElement = this.template.querySelector('lightning-record-edit-form');
+        console.log(formElement);
+        formElement.reset();
+    }
     
 }
