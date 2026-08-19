1020e59e750a4e07bdf86da3de3082b0555cf8da
hayadav <113333121+harshit8756@users.noreply.github.com>
2026-08-19 14:42:54 +0530

    create form with Events handlings

--- force-app/main/default/lwc/caseFormWithEventsHandlingLDS/caseFormWithEventsHandlingLDS.js
+++ force-app/main/default/lwc/caseFormWithEventsHandlingLDS/caseFormWithEventsHandlingLDS.js
@@ -2,4 +2,20 @@ import { LightningElement } from 'lwc';
 
 export default class CaseFormWithEventsHandlingLDS extends LightningElement {
     objectApiName = 'Case';
+    handleSubmit(event){
+        console.log('Form Submitted for case creation' + event.detail);
+    }
+
+    handleSuccess(event){
+        console.log();
+        alert('Case has been successfully created' + event.detail);
+    }
+
+    handleError(event){
+        console.log();
+    }
+
+    handleLoad(event){
+        console.log();
+    }
 }
