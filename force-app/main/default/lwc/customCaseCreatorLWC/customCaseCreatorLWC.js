3e245251cce556c218992f2f2e1d1cc6ea0bb230
hayadav <113333121+harshit8756@users.noreply.github.com>
2026-08-24 14:09:35 +0530

    create custom case

--- force-app/main/default/lwc/customCaseCreatorLWC/customCaseCreatorLWC.js
+++ force-app/main/default/lwc/customCaseCreatorLWC/customCaseCreatorLWC.js
@@ -1,4 +1,9 @@
 import { LightningElement } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
+import CASE_OBJECT from '@salesforce/schema/Case';
+import SUBJECT from '@salesforce/schema/Case.Subject';
+import PRIORITY from '@salesforce/schema/Case.Priority';
+import DESCRIPTION from '@salesforce/schema/Case.Description';
 
 export default class CustomCaseCreatorLWC extends LightningElement {
     subject = '';
@@ -34,8 +39,28 @@ export default class CustomCaseCreatorLWC extends LightningElement {
 
     // I have the vaues and now I have to create the case record using the createRecord method so that we can create the case record in the Salesforce database
     // without calling apex or server side code we can create the record using the createRecord method
-    handleCreateCase() {
-        
+    async handleCreateCase() {
+
+        const fields = {};
+        //Key value pair
+        fields[SUBJECT.fieldApiName] = this.subject;
+        fields[PRIORITY.fieldApiName] = this.priority;
+        fields[DESCRIPTION.fieldApiName] = this.description;
+
+        // object api bname and the list of fields
+        // Case Object and fields : Subject , Priority , description
+        let recrodInput = { apiName: CASE_OBJECT.objectApiName, fields };
+        const caseRecord = await createRecord(recrodInput)
+        .then((record) => {
+
+        })
+            .catch((err) => {
+
+            });
+
+        // As soon as the record is create or error will come you actually want to alert it
+
+
     }
 
 }
