508c137a5e120a0745ada77a086e027868b97bcd
hayadav <113333121+harshit8756@users.noreply.github.com>
2026-09-01 00:27:14 +0530

    multiple wires examples

--- force-app/main/default/lwc/multipleWiresDependentAndIndependent/multipleWiresDependentAndIndependent.js
+++ force-app/main/default/lwc/multipleWiresDependentAndIndependent/multipleWiresDependentAndIndependent.js
@@ -1,6 +1,27 @@
-import { LightningElement } from 'lwc';
-
+import { LightningElement, wire } from 'lwc';
+import getAccounts from '@salesforce/apex/multipleWiresController.getAccounts';
+import getContacts from '@salesforce/apex/multipleWiresController.getContacts';
 export default class MultipleWiresDependentAndIndependent extends LightningElement {
-    getAccountList;
-    getContactList;
+    getAccountList = [];
+    getContactList = [];
+
+    // #wire 1
+    @wire(getAccounts)
+    wireAccounts({data,error}){
+        if(data){
+            this.getAccountList = data;
+        }else if(error){
+            console.error('Account Error : ' + error);   
+        }
+    }
+
+    // #wire 2
+    @wire(getContacts)
+    wiredContacts({data,error}){
+        if(data){
+            this.getContactList = data;
+        }else if(error){
+            console.error('Contact Error : ' + error);   
+        }
+    }
 }
---
+++ force-app/main/default/lwc/multipleWiresDependentAndIndependent2/multipleWiresDependentAndIndependent2.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class MultipleWiresDependentAndIndependent2 extends LightningElement {}
