0d1b61014ce157e34e51fe7d92a79aa8be38c968
hayadav <113333121+harshit8756@users.noreply.github.com>
2026-09-01 12:18:26 +0530

    toast notification in LWC

--- force-app/main/default/lwc/toastNotificationInLWC/toastNotificationInLWC.js
+++ force-app/main/default/lwc/toastNotificationInLWC/toastNotificationInLWC.js
@@ -1,3 +1,40 @@
 import { LightningElement } from 'lwc';
+import createTask from '@salesforce/apex/toastMessageController.createTask';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
-export default class ToastNotificationInLWC extends LightningElement {}
+export default class ToastNotificationInLWC extends LightningElement {
+
+    subject = '';
+    //Get value from Input
+    handleSubjectChange(event){
+        this.subject = event.target.value;
+    }
+
+    // Calling apex
+    handleCreateTask(){
+        createTask({subject : this.subject})
+
+        //success
+        .then(result => {
+            console.log('Task Create : ' + result);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title : 'Success',
+                    message : 'Task Created Successfully !',
+                    variant : 'success'
+                })
+            );
+        })
+        .catch(error => {
+            console.error('Error : ' + error);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title : 'Error',
+                    message : error.body ?.message  || 'Something went wrong',
+                    variant : error
+                })
+            );
+        });
+
+    }
+}
